e564fbe474508dab4992919fb72790ec2f2dab09
Lu <jlu.fernandez90@gmail.com>
2023-10-02 21:38:55 +0200

    carrito

--- src/services/ProductsServices.js
+++ src/services/ProductsServices.js
@@ -2,6 +2,8 @@ import createHttp from "./BaseServices";
 const http = createHttp(true);
 
 
-export const getProductList = () => http.get('/');
+export const getProductList = () => http.get('/');  
 
 export const getProductDetails = (id) => http.get(`/products/${id}`);
+
+export const buyProduct = (products) => http.post('/products/checkout', products);
