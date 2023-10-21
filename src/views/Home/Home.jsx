db16db5efb00854596747e2323820100430de84e
Lu <jlu.fernandez90@gmail.com>
2023-10-21 09:25:31 +0200

    banner en home

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -3,6 +3,7 @@ import ImagesList from "../Images/ImagesList";
 import ImageSliderComponent from "../../components/Slider/Slider";
 import ProductsList from '../Products/ProductsList';
 import Categories from "../../components/Categories/Categories";
+import { Link } from "react-router-dom";
 
 
 const Message = ({ message }) => (
@@ -17,6 +18,11 @@ const Home = () => {
      <ImageSliderComponent/>
      <Categories/>
      <ProductsList />
+     <div className="Banner">
+        <Link to="/tool-landing">
+          <img src="/img/muestra.png" alt="muestra" className="muestra" />
+        </Link>
+      </div>
      <ImagesList/>
     
 
